255c5008adad2fc36e5f009293c96e6a023d8699
oLOWONLA <olowonlafaruq@gmail.com>
2024-02-01 18:57:58 +0100

    -nikeClone

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import Nav from './components/Nav'
+import { Hero,
+  PopularProducts,
+  SuperQuality,
+  Service,
+  SpecialOffer,
+  CustomerReview,
+  Subscribe,
+  Footer} from './sections/index'
+
+function App() {
+  
+
+  return (
+    <>
+       <main className="relative">
+          <Nav/>
+          <section className="xl:padding-l wide:padding-r padding-b">
+            <Hero/>
+          </section>
+          <section className="padding">
+           <PopularProducts  />
+          </section>
+
+          <section className="padding">
+           <SuperQuality/>
+          </section>
+
+          <section className="padding-x py-10">
+           <Service/>
+          </section>
+
+          <section className="padding">
+           <SpecialOffer/>
+          </section>
+
+          <section className="bg-pale-blue padding">
+           <CustomerReview/>
+          </section>
+
+          <section className="padding-x sm:py-32  py-16 w-full">
+           <Subscribe/>
+          </section>
+          <section className="bg-black padding-x padding-t pb-8">
+            <Footer/>
+          </section>
+       </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "../sections/Hero";
+import PopularProducts from "../sections/PopularProduct";
+import SuperQuality from "../sections/SuperQuality";
+import Service from "./Service";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReview from "./CustomerReview";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Service,
+    SpecialOffer,
+    CustomerReview,
+    Subscribe,
+    Footer
+}
